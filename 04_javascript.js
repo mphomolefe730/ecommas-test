fad3075e9dd59878d8a61e5b959a024d3eb554a3
Mpho Molefe <mphomolefe730@gmail.com>
2023-07-23 00:18:59 +0200

    Changes to be committed: 	modified:   04_javascript.js 	modified:   04_javascript.ts 	-changed init to loadproducts 	-added a 2second delay to buyButton function 	-add cart array to sort purchase items 	---made buyButton function 	-displays all buys in a node list 	-used foreach method to access every button 	-access the id named under data-id, and sort in a variabale 	-variable to check if cart has id of product 	+if it does, change button content to "in cart" and disable the 	button 	+adddeventlistener to listen for click evens 	+if click, change button content to "in cart" and disable button 	modified:   04_styles.css 	made purchase button from id to class

--- 04_javascript.js
+++ 04_javascript.js
@@ -1,29 +1,27 @@
-var _a;
 var productList = [{ name: "air force 1", Description: "white shoes", price: 1200, id: "peoirj" },
     { name: "jordan 13", Description: "blue, white and black shoes", price: 8000, id: "djhgs" },
     { name: "JBL bt500", Description: "wireless bluetooth earphones", price: 800, id: "2j2h2t" },
     { name: "shield rollon", Description: "dry sport shield rollon", price: 20, id: "sdj283" },
     { name: "vaseline", Description: "vaseline tab, kid size", price: 25, id: "2ieudhv" },
-    { name: "air force 1", Description: "Casio Calculator", price: 600, id: "sbhdv2t6" },];
+    { name: "Casio Calculator", Description: "Casio Calculator", price: 600, id: "sbhdv2t6" },];
 var removeCartItemButtons = document.getElementsByClassName('kill');
-var addCartItemButtons = document.getElementsByClassName('purchase-btn');
+var productDOM = document.getElementById('productItemsDisplay');
+//document.getElementById('productItemsDisplay')!.innerHTML=productItemDisplayContectMaker();
+var cart = ["peoirj"];
 console.log(removeCartItemButtons);
-console.log(addCartItemButtons);
-document.getElementById('productItemsDisplay').innerHTML = productItemDisplayContectMaker();
-(_a = document.getElementById('purchase-btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
-    console.log('clicked');
-});
-function productItemDisplayContectMaker() {
+loadProducts();
+setTimeout(buyButton, 2000);
+function loadProducts() {
     var pageForItems = '';
     for (var i = 0; i < productList.length; i++) {
         pageForItems += (createItem(i));
-    } //function to initialise the page of items
-    return pageForItems;
+    }
+    productDOM.innerHTML = pageForItems;
 }
 ;
 function createItem(indexOfProduct) {
     var itemcontainer = '<div class="item">';
-    itemcontainer += '<img id="product-image"/>';
+    itemcontainer += '<img id="product-image" alt="Product Image"/>';
     itemcontainer += createItemDescription(indexOfProduct);
     itemcontainer += '</div>';
     return itemcontainer;
@@ -34,12 +32,29 @@ function createItemDescription(indexOfProduct) {
     itemDescriptionContainer += "<h2 title=\"".concat(productList[indexOfProduct].name, "\" id=\"productname\"> ").concat(productList[indexOfProduct].name, " </h2>");
     //name is the 1st element in productlist array
     itemDescriptionContainer += "<p id=\"productdescription\"> ".concat(productList[indexOfProduct].Description, " </p>"); //description
-    var priceAndPurchase = "<div id=\"productprice\">\n    <span> ".concat(productList[indexOfProduct].price, "</span>\n    <button id=\"purchase-btn\">PURCHASE</button>\n    </div>");
+    var priceAndPurchase = "<div id=\"productprice\">\n    <span> ".concat(productList[indexOfProduct].price, "</span>\n    <button class=\"purchase-btn\" data-id=\"").concat(productList[indexOfProduct].id, "\">PURCHASE</button>\n    </div>");
     itemDescriptionContainer += priceAndPurchase;
     itemDescriptionContainer += '</div>'; //div
     return itemDescriptionContainer;
 }
 ;
+function buyButton() {
+    //list all purchase button| is a Nodelist
+    var buttons = document.querySelectorAll(".purchase-btn");
+    buttons.forEach(function (button) {
+        var id = button.getAttribute('data-id'); //get the id(data-id) of each product
+        var inCart = cart.some(function (item) { return item === id; }); //checks if id is in cart
+        if (inCart) {
+            button.textContent = "In Cart"; //change text for purchase button
+            button.setAttribute('disabled', 'disabled'); //makes the button inactive
+        }
+        button.addEventListener('click', function () {
+            button.textContent = "In Cart";
+            button.setAttribute('disabled', 'disabled');
+        });
+    });
+    console.log(buttons);
+}
 function createCartItem() {
     var cartItemContainer = "<div id=\"cart-item-holder\">"; //div
     cartItemContainer += "<div class=\"quantity\"><p>1</p></div>"; //quantity
--- 04_javascript.ts
+++ 04_javascript.ts
@@ -3,31 +3,29 @@ let productList = [{name: "air force 1", Description: "white shoes",price: 1200,
 {name: "JBL bt500", Description: "wireless bluetooth earphones",price: 800, id: "2j2h2t"},
 {name: "shield rollon", Description: "dry sport shield rollon",price: 20, id: "sdj283"},
 {name: "vaseline", Description: "vaseline tab, kid size",price: 25, id: "2ieudhv"},
-{name: "air force 1", Description: "Casio Calculator",price: 600, id: "sbhdv2t6"},];
+{name: "Casio Calculator", Description: "Casio Calculator",price: 600, id: "sbhdv2t6"},];
 
 let removeCartItemButtons = document.getElementsByClassName('kill');
-let addCartItemButtons = document.querySelectorAll('purchase-btn');
+const productDOM = document.getElementById('productItemsDisplay');
+//document.getElementById('productItemsDisplay')!.innerHTML=productItemDisplayContectMaker();
+let cart=["peoirj"];
 
 console.log(removeCartItemButtons);
-console.log(addCartItemButtons);
 
-document.getElementById('productItemsDisplay')!.innerHTML=productItemDisplayContectMaker();
-document.getElementById('purchase-btn')?.addEventListener('click',()=>{
-    console.log('clicked');
-});
+loadProducts();
+setTimeout(buyButton,2000);
 
-
-function productItemDisplayContectMaker():string{
+function loadProducts():void{                                          //function to initialise the page of items
     let pageForItems:string = '';
     for(let i=0; i<productList.length;i++){
         pageForItems+=(createItem(i));
-    }                                               //function to initialise the page of items
-    return pageForItems;
+    }                                               
+    productDOM!.innerHTML=pageForItems;
 };
 
 function createItem(indexOfProduct:number):string{
     let itemcontainer:string = '<div class="item">';
-    itemcontainer+='<img id="product-image"/>'
+    itemcontainer+='<img id="product-image" alt="Product Image"/>'
     itemcontainer+=createItemDescription(indexOfProduct);
     itemcontainer+='</div>'; 
 
@@ -42,7 +40,7 @@ function createItemDescription(indexOfProduct):string{
     
     let priceAndPurchase =`<div id="productprice">
     <span> ${productList[indexOfProduct].price}</span>
-    <button id="purchase-btn">PURCHASE</button>
+    <button class="purchase-btn" data-id="${productList[indexOfProduct].id}">PURCHASE</button>
     </div>`
     
     itemDescriptionContainer+=priceAndPurchase;
@@ -50,6 +48,26 @@ function createItemDescription(indexOfProduct):string{
 
     return itemDescriptionContainer;
 };
+
+function buyButton(){
+    //list all purchase button| is a Nodelist
+    const buttons = document.querySelectorAll(".purchase-btn");
+    
+    buttons.forEach(button=>{
+        let id = button.getAttribute('data-id');            //get the id(data-id) of each product
+        let inCart = cart.some(item=> item === id);         //checks if id is in cart
+
+        if (inCart){
+            button.textContent="In Cart";                   //change text for purchase button
+            button.setAttribute('disabled','disabled');     //makes the button inactive
+        }
+        button.addEventListener('click', () => {
+            button.textContent="In Cart";
+            button.setAttribute('disabled','disabled');
+        })
+    })
+    console.log(buttons);
+}
 
 function createCartItem():string{
     let cartItemContainer:string= `<div id="cart-item-holder">`;//div
